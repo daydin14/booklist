3bfe571ee424e2837ed01b7f4749132edaee588c
David Aydin <david.aydin6@gmail.com>
2022-05-24 12:36:29 -0400

    Created create route

--- server.js
+++ server.js
@@ -15,4 +15,13 @@ db.on("error", (err) => console.log(err.message + " is mongo not running?"));
 db.on("connected", () => console.log("mongo connected"));
 db.on("disconnected", () => console.log("mongo disconnected"));
 
+// Create
+app.post("/books", (req, res) => {
+  res.send("received");
+});
+// Create
+// app.post("/books", (req,res) => {
+//     res.send(req.body);
+// });
+
 app.listen(PORT);
