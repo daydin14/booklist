894fae4bc06b1f8580a9663c5c82bc77c63fe869
David Aydin <david.aydin6@gmail.com>
2022-05-24 12:29:27 -0400

    Connect Express to MongoDB Atlas

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-
 const PORT = process.env.PORT;
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on("error", (err) => console.log(err.message + " is mongo not running?"));
+db.on("connected", () => console.log("mongo connected"));
+db.on("disconnected", () => console.log("mongo disconnected"));
+
 app.listen(PORT);
